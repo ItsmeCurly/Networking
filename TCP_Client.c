c4a04963fa5502e6211feaeb032db3283d7f427a
ItsmeCurly <adamg765@gmail.com>
2020-10-08 15:33:53 -0400

    update

---
+++ TCP_Client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h> //inet_addr
+#include <unistd.h>    //write
+#include <netdb.h>
+
+struct sockaddr_in sa;
+int main()
+{
+    FILE* fp = fopen("new.txt", "w");
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(10059);
+    inet_pton(AF_INET, "10.0.2.15", &(sa.sin_addr));    //set ip address of server here
+
+    printf("Client connecting...\n");
+    int x = connect(sock, (struct sockaddr *)&sa, sizeof(sa));
+
+    char filesize[100];
+
+    recv(sock, filesize, 100, 0);
+
+    printf("%s\n", filesize);   //print information regarding file size
+
+    while(1) {
+        char buf[1024]; //set buffer to 1024 bytes, as we won't receive more than that
+        int recv_size = recv(sock, buf, 1024, 0);
+
+        const char *ptr = strchr(buf, '\0');    //search buffer for null terminating character (array was initialized with \0)
+        if(ptr) {
+            int index = ptr - buf;  //get index of \0
+            
+            fwrite(buf, sizeof(char), index, fp);   //write up to the first null terminating character
+
+            if (index < 1023) { //if null terminating character is not at end of buf then there is more to go
+                break;
+            }
+        }
+    }
+    printf("File contents successfully written\n");
+    fclose(fp);
+
+    printf("\nEnd of message\n");
+}
---
+++ TCP_Server.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <errno.h>
+
+int getfilesize(char*);
+
+int send_size, recv_size;
+int main(int argc, char *argv[])
+{
+    int socket_desc, client_sock, c, read_size; //sockets and such
+    struct sockaddr_in server, client;          //one for listening one for connection with client(s)
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    client_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    char* filename = "file.txt";    //set file name to send here
+
+    printf("Sockets created\n");
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(10059);
+    inet_pton(AF_INET, "10.0.2.15", &(server.sin_addr)); //set ip address of server here
+
+    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+
+        perror("bind failed. Error"); //perror is a very helpful function for tracking down errors
+        return 1;
+    }
+    printf("Bind completed \n");
+
+    c = sizeof(struct sockaddr_in);
+
+    listen(socket_desc, 1);
+
+    printf("Waiting for incoming connections...\n");
+
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c);
+    //if successful, client is filled in with address of connecting socket
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+
+    printf("Connection accepted\n");
+
+    char st[100] = "The size of the file is ";
+
+    int size = getfilesize(filename);
+    char size_buf[50];
+    sprintf(size_buf, "%d", size);
+
+    strcat(st, size_buf);
+    char* _b = " bytes.";
+    strcat(st, _b); //convoluted strcat to get string i want
+
+    send(client_sock, st, 100, 0);
+
+    char buf[1024]; //set buf to 1024 bytes
+    memset(buf, '\0', sizeof(char) * 1024);
+
+    FILE *fp;
+
+    fp = fopen(filename, "r");
+    
+    int count = 0;
+    while(1) {
+        char c = fgetc(fp);
+
+        if (c == EOF) {
+            buf[count+1] = '\0';
+
+            send(client_sock, buf, 1024, 0);
+
+            break;
+        }
+
+        buf[count] = c;
+        count+= 1;
+
+        if (count == 1023) {    //if reached buf size - 1, stop and send
+            send(client_sock, buf, 1024, 0);
+
+            count = 0;
+
+            memset(buf, '\0', sizeof(char) * 1024);
+        }
+    }
+
+    fclose(fp);
+
+    close(socket_desc);
+    close(client_sock);
+    return 0;
+}
+
+int getfilesize(char* filename) {
+    int count = 0;
+    FILE* fp = fopen(filename, "r");
+
+    while (1) {
+        char c = fgetc(fp);
+
+        if (c == EOF) {
+            break;
+        }
+        count += 1;
+    }
+
+    fclose(fp);
+
+    return count;
+}
