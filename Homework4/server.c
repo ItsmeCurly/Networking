d43d2e8d79ecae68b383dc9c3f63bbce4487236d
Adam Green <31974406+ItsmeCurly@users.noreply.github.com>
2020-11-18 12:09:44 -0500

    Update server.c

--- Homework4/server.c
+++ Homework4/server.c
@@ -6,9 +6,13 @@
 #include <netdb.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <pthread.h> 
+#include <semaphore.h> 
 
-#define s_IP "130.111.46.105"
-#define s_PORT 45022
+#define server_IP "130.111.46.105"
+#define server_TCP_PORT 45022
+#define server_UDP_PORT 45023
+#define NUM_BIND_TRIES 5
 
 char* concat(const char*, const char*);
 void *tcp_thread(void *ptr);
@@ -19,32 +23,29 @@ struct msg {
     int val;
 };
 
+struct sockaddr_in client;
+
+sem_t mutex;
+
 int main(int argc, char *argv[]) {
-    int tcp_sock, udp_sock;
+    int udp_sock;
     socklen_t addrLen;
-    struct sockaddr_in udp_server, tcp_server, udp_client, tcp_client;
+    struct sockaddr_in udp_server;
     
     if((udp_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
         perror("Error: socket creation failed\n");
         exit(0);
     }
 
-    if((tcp_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Error: socket creation failed\n");
-        exit(0);
-    }
+    udp_server.sin_family = AF_INET;
+    udp_server.sin_port = htons(s_UDP_PORT);
+    udp_server.sin_addr.s_addr = INADDR_ANY;
 
-    server.sin_family = AF_INET;
-    server.sin_port = htons(s_PORT);
-    server.sin_addr.s_addr = INADDR_ANY;
-    //inet_pton(AF_INET, s_IP, &(server.sin_addr));
-
-    #define NUM_TRIES 5
     int j = 1;
 
-    printf("Binding...\n");
+    printf("Binding UDP socket...\n");
 
-    while (bind(m_sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
+    while (bind(udp_sock, (struct sockaddr *) &udp_server, sizeof(udp_server)) < 0) {
         if (j == 1) {
             perror("Error: Bind failed");
         }
@@ -64,22 +65,75 @@ int main(int argc, char *argv[]) {
         j+=1;
     }
 
-    printf("Bind completed\n");
+    printf("UDP Bind completed\n");
 
+    int tcp_sock;
+    socklen_t addrLen;
+    struct sockaddr_in tcp_server;
+
+    if((tcp_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Error: socket creation failed\n");
+        exit(0);
+    }
+
+    tcp_server.sin_family = AF_INET;
+    tcp_server.sin_port = htons(s_TCP_PORT);
+    inet_pton(AF_INET, s_IP, &(tcp_server.sin_addr));
+    
+    int j = 1;
+
+    printf("Binding TCP socket...\n");
+
+    while (bind(tcp_sock, (struct sockaddr *) &tcp_server, sizeof(tcp_server)) < 0) {
+        if (j == 1) {
+            perror("Error: Bind failed");
+        }
+        else {
+            char* temp = "Attempt";
+            char* m_b = malloc(strlen(temp) + 8);
+
+            sprintf(m_b, "%s #%d", temp, j);
+            
+            perror(m_b);
+        }
+        sleep(2);   //attempt to bind sometimes fails, set it so that it waits 2 seconds after every failed bind, up to 5 attempts
+
+        if (j >= NUM_TRIES) {
+            exit(0);
+        }
+        j+=1;
+    }
+
+    printf("TCP Bind completed\n");
+
+    //start threads
+
+    pthread_t udp, tcp;
+    udp = pthread_create(&udp, NULL, udp_thread, (void*) udp_sock);
+    tcp = pthread_create(&tcp, NULL, tcp_thread, (void*) tcp_sock);
+}
+
+
+
+void *tcp_thread(void* ptr) {
+    
+}
+
+void *udp_thread(void* ptr) {
     printf("Waiting for response...\n");
 
     char temp[12];
 
     addrLen = sizeof(client);
 
-    if (recvfrom(m_sock, temp, sizeof(temp), 0, (struct sockaddr *) &client, &addrLen) < 0) { //receive initialization message from client
+    if (recvfrom(udp_sock, temp, sizeof(temp), 0, (struct sockaddr *) &client, &addrLen) < 0) { //receive initialization message from client
         printf("Error receiving message from client");
         exit(1);
     }
 
     printf("Initialization message received from %s at %d port \n", inet_ntoa(client.sin_addr), htons(client.sin_port));
 
-    //create array of integers to send
+    // create array of integers to send
 
     #define ARR_SIZE 10000
 
@@ -97,22 +151,10 @@ int main(int argc, char *argv[]) {
         m_msg.chunkNum = i;
         m_msg.val = arr[i];
         
-        if (sendto(m_sock, &m_msg, sizeof(m_msg), 0, (struct sockaddr *) &client, addrLen) < 0) {
+        if (sendto(udp_sock, &m_msg, sizeof(m_msg), 0, (struct sockaddr *) &client, addrLen) < 0) {
             perror("A message was not sent correctly");\
         }
     }
 
     printf("Data sent successfully\n");
-
-    close(m_sock);
-}
-
-
-
-void *tcp_thread(void* ptr) {
-    
-}
-
-void *udp_thread(void* ptr) {
-    
 }
